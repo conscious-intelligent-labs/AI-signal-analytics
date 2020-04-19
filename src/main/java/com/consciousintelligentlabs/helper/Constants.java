d4d2d0f633eaa4620115c70f08cfd21b4b34e62d
Dr. Kyle Morton <kfmorton@drs-macbook-pro.home>
2020-04-19 16:36:19 -0500

    Added more API calls and rabbit

--- src/main/java/com/consciousintelligentlabs/helper/Constants.java
+++ src/main/java/com/consciousintelligentlabs/helper/Constants.java
@@ -12,8 +12,12 @@ public class Constants {
   public static final String SUPPORT_AND_RESISTANCE_EVENT = "[SupportAndResistance]";
   public static final String TECHNICAL_INDICATOR_EVENT = "[TechnicalIndicator]";
   public static final String NEWS_EVENT = "[News]";
+  public static final String MARKETDATA_EVENT = "[MarketData]";
   public static final String[] CHART_TIMEFRAMES = {
     "1", "5", "15", "30", "60", "240", "D", "W", "M"
   };
   public static final String[] NEWS_CATAGORY = {"general", "forex", "crypto", "merger"};
+  public static final String[] ACCEPTED_INDICATORS = {
+          "SMA", "EMA", "MACD", "RSI", "ADX", "CCI", "DX", "STOCH"
+  };
 }
--- src/main/java/com/consciousintelligentlabs/service/impl/TechnicalIndicatorServiceImpl.java
+++ src/main/java/com/consciousintelligentlabs/service/impl/TechnicalIndicatorServiceImpl.java
@@ -31,6 +31,12 @@ public class TechnicalIndicatorServiceImpl implements TechnicalIndicatorService 
   @Value(value = "${harmonicpattern.exchanges}")
   private String exchanges;
 
+  @Value(value = "${alphaAdvantage.api.key}")
+  protected String alphavantageApiKey;
+
+  @Value(value = "${alphaAdvantage.api.endpoint}")
+  protected String alphavantageEndpoint;
+
   /**
    * Creates the endpoint.
    *
@@ -53,10 +59,8 @@ public class TechnicalIndicatorServiceImpl implements TechnicalIndicatorService 
    * Makes a request to get the Technical Indicators for a Symbol.
    *
    * @return APIResponse
-   * @throws Exception
    */
-  public APIResponse getTechnicalIndicatorService(String symbol, String timeframe)
-      throws Exception {
+  public APIResponse getTechnicalIndicatorData(String symbol, String timeframe) {
     // Create response object.
     APIResponse APIResponse = new APIResponse();
     HTTPClient client = new HTTPClient();
@@ -87,5 +91,115 @@ public class TechnicalIndicatorServiceImpl implements TechnicalIndicatorService 
     }
 
     return APIResponse;
+  }
+
+  /**
+   * Gets the data for a single indicator.
+   *
+   * @param indicator  Indicator.
+   * @param symbol     Symbol.
+   * @param timeframe  Timeframe.
+   * @param count      Candles to return.
+   * @param seriesType Price series type.
+   *
+   * @return
+   */
+  public APIResponse getSingleIndicatorData(String indicator, String symbol, String timeframe, int count, String seriesType) {
+
+    // Create response object.
+    APIResponse APIResponse = new APIResponse();
+
+    String convertedTimeframe = this.convertTimeframe(timeframe);
+    String url = "function=" +
+            indicator +
+            "&symbol=" +
+            symbol +
+            "&interval=" +
+            convertedTimeframe +
+            "&time_period=" +
+            count +
+            "&series_type=" +
+            seriesType +
+            "&apikey=" + this.alphavantageApiKey;
+    try {
+      // Get Client.
+      HTTPClient httpClient = new HTTPClient();
+      httpClient.createGetClient(this.alphavantageEndpoint + url);
+      httpClient.sendGet();
+
+      logger.info(Constants.MARKETDATA_EVENT + " Result returned: " + httpClient.getResult());
+
+      // Set response to return to server.
+      APIResponse.setCode(HttpStatus.OK);
+      APIResponse.setMessage(httpClient.getResult());
+    } catch (Exception e) {
+      logger.error(
+              Constants.MARKETDATA_EVENT
+                      + " Error making an request to retrieve Market data from "
+                      + this.alphavantageEndpoint
+                      + ". [Error] "
+                      + e.getMessage());
+
+      // Set Error response to return to server.
+      APIResponse.setCode(HttpStatus.SERVICE_UNAVAILABLE);
+      APIResponse.setMessage("Internal Error. Please see admin for more details.");
+    }
+
+    return APIResponse;
+  }
+
+  /**
+   * Parses a Symbol.
+   *
+   * @param symbol Symbol.
+   * @return String []
+   */
+  public String[] parseSymbolToArray(String symbol) {
+    int indexOfSplit = symbol.indexOf('/');
+    String symbolFrom;
+    String symbolTo;
+
+    if (indexOfSplit > 0) {
+      symbolFrom =  symbol.substring(0, indexOfSplit);
+      symbolTo =  symbol.substring(indexOfSplit + 1);
+    } else {
+      return new String[] {symbol};
+    }
+
+    return new String[]{symbolFrom, symbolTo};
+  }
+
+  /**
+   * Converts the timeframe.
+   *
+   * @param timeframe Timeframe to convert.
+   *
+   * @return
+   */
+  private String convertTimeframe (String timeframe) {
+    switch (timeframe.toLowerCase()) {
+      case "m1":
+        return "1min";
+      case "m5":
+        return "5min";
+      case "m15":
+        return "15min";
+      case "m30":
+        return "30min";
+      case "h1":
+        return "60min";
+      case "1":
+        return "1min";
+      case "5":
+        return "5min";
+      case "15":
+        return "15min";
+      case "30":
+        return "30min";
+      case "60":
+        return "60min";
+
+      default: return "15min";
+    }
   }
 }
